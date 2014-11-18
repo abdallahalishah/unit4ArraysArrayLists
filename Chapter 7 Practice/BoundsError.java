0c8d82560db11e64005879bb5c4cbc731b7da106
abdallahalishah <idontknow.abdallah@gmail.com>
2014-11-17 22:13:50 -0600

    chapter 7 moved to this folder practice

---
+++ Chapter 7 Practice/BoundsError.java
@@ -0,0 +1,13 @@
+public class BoundsError
+{
+    public static void main(String[] args)
+    {
+        int[] scores = new int[10];
+        
+        for (int i = 0; i < 10; i++)
+        {
+            scores[i] = i+1;
+        }
+        
+    }
+}
